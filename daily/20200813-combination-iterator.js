f4adb27d22cbe8ee2bdd41d5ea1e97dd57b29862
kapppa-joe <kapppa.joe@gmail.com>
2020-08-13 18:33:52 +0800

    20200813 daily exercise

--- daily/20200813-combination-iterator.js
+++ daily/20200813-combination-iterator.js
@@ -3,8 +3,8 @@
  * @param {number} combinationLength
  */
 var CombinationIterator = function(characters, combinationLength) {
-    this._chars = characters.split('');
-    this._maxBit = 2 ** (characters.length - 1);
+    let chars = characters.split('');
+    let maxBit = 2 ** (characters.length - 1);
 
     const countBits = (n) => {
        let i;
@@ -12,30 +12,22 @@ var CombinationIterator = function(characters, combinationLength) {
        return i
     }
 
+    const bitFlagsToString = (flags) => {
+        return chars.filter((_, i) => (maxBit >> i | flags) === flags).join('');
+    }
+    
     this._pool = Array(2 ** characters.length).fill()
                     .map((_, int) => int)
-                    .reverse()
-                    .filter(bitFlags => countBits(bitFlags) === combinationLength);
-    this._curr = 0;
-};
+                    .filter(bitFlags => countBits(bitFlags) === combinationLength)
+                    .map(bitFlagsToString);
 
-/**
- * @param {n} number (a number to store bitflags for combination)
- * @return {string}
- */
-CombinationIterator.prototype._bitFlagsToString = function(flags) {
-    return this._chars.filter((_, i) => (this._maxBit >> i | flags) === flags).join('');
 };
-
 
 /**
  * @return {string}
  */
 CombinationIterator.prototype.next = function() {
-    if (this._curr >= this._pool.length) return "";
-    let result = this._bitFlagsToString(this._pool[this._curr]);
-    this._curr++;
-    return result;
+    return this._pool.pop();
 };
 
 
@@ -43,7 +35,7 @@ CombinationIterator.prototype.next = function() {
  * @return {boolean}
  */
 CombinationIterator.prototype.hasNext = function() {
-    return this._curr < this._pool.length;
+    return this._pool.length > 0;
 };
 
 /** 
