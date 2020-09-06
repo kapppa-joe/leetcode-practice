935c22614ef3e161a97b959785491bbd360c6b70
kapppa-joe <kapppa.joe@gmail.com>
2020-09-06 20:17:58 +0800

    20200906 daily exercise final

--- daily/20200906-image-overlap.js
+++ daily/20200906-image-overlap.js
@@ -6,19 +6,16 @@
 var largestOverlap = function(A, B) {
     let n = A.length;
     let scores = Array(4 * n * n).fill(0);
-    //let scores = {};
 
+    // i, j: coordinate ;  x, y: translation 
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
             for (let x = -i; x + i < n; x++) {
                 for (let y = -j; y + j < n; y++) {
-                    //if (!scores[x + ' '+ y]) scores[x + ' '+ y] = 0;
-                    //scores[x + ' '+ y] += A[i][j] * B[i + x][j + y];
                     scores[(x + n - 1) * (2 * n -1) + (y + n - 1)] += A[i][j] * B[i + x][j + y];
                 }
             }
         }
     }
-    return Math.max(...scores); //console.log(scores);
-    //return scores.reduce((score, max) => score > max ? score : max);
+    return Math.max(...scores);
 };
