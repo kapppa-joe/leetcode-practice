d87fa2ac36b3092574680003efcc30d853e09668
kapppa-joe <kapppa.joe@gmail.com>
2020-09-07 20:30:54 +0800

    daily exercise 20200907

---
+++ daily/20200907-word-pattern.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} pattern
+ * @param {string} str
+ * @return {boolean}
+ */
+var wordPattern = function(pattern, str) {
+    let strArr = str.split(' ');
+    if (strArr.length !== pattern.length) return false
+
+    let map = {};
+    let revMap = {};
+
+    for (let i in strArr) {
+        if (!map[pattern[i]]) {
+            map[pattern[i]] = strArr[i];
+            if (revMap[strArr[i]]) return false;
+            revMap[strArr[i]] = pattern[i];
+        } else {
+            if (map[pattern[i]] !== strArr[i] || revMap[strArr[i]] !== pattern[i]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+};
