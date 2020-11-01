144f8819fbdb60ceeeef7e980e0060730914e77c
kapppa-joe <kapppa.joe@gmail.com>
2020-11-01 15:22:03 +0800

    20201101 daily practice

---
+++ daily/20201101_convert_binary_num_in_linked_list.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        num = 0
+        node = head
+        while node:
+            num = (num << 1) | node.val
+            node = node.next
+        return num
